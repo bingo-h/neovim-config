9735862cef4136a9731310262566dae7e355b281
LoveSun-Knight <2971859500@qq.com>
2025-04-24 13:16:51 +1000

    add config.toml - for Neovide

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,8 +1,8 @@
 -- vim.opt
 -- Tab
 vim.opt.tabstop = 4  -- number of spaces per TAB in visual mode
-vim.opt.softtabstop = 4  -- number of spaces per TAB in editing mode
-vim.opt.shiftwidth = 4  -- number of spaces per TAB in insert mode
+-- vim.opt.softtabstop = 4  -- number of spaces per TAB in editing mode
+-- vim.opt.shiftwidth = 4  -- number of spaces per TAB in insert mode
 vim.opt.expandtab = true  -- tabs are spaces
 
 -- UI config
@@ -11,9 +11,11 @@ vim.opt.number = true  -- show absolute number
 -- Smart indent
 vim.opt.smartindent = false
 
+-- Font
+-- vim.opt.guifont = "MonaspiceRn_nerd_font_mono:h18"
+
 
 -- vim.global
 -- Netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
